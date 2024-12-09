e4fef6b5aa5f6bc1d43ae2a1391b1e3be500f65d
PNS <solovjov_pn@mail.ru>
2024-12-09 15:54:09 +0300

    start

---
+++ site-grabber/src/main/java/pn/sites/site_grabber/SiteGrabberApplication.java
@@ -0,0 +1,13 @@
+package pn.sites.site_grabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteGrabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteGrabberApplication.class, args);
+	}
+
+}
